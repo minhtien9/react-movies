48fb9167b6d05ccf1ab8dbce6bbd55223fe19e56
DESKTOP-PEKI6LH\Lenovo <tienvm1@bytetech.io>
2022-12-03 19:30:00 +0700

    first commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <header className='header flex items-center justify-center gap-x-5 text-white py-10 mb-5'>
+                <span className='text-primary'>Home</span>
+                <span>Movies</span>
+            </header>
+            <section className='banner h-[500px] page-container mb-20'>
+                <div className='w-full h-full rounded-lg relative '>
+                    <div className='overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg'></div>
+                    <img
+                        src='https://nld.mediacdn.vn/2019/4/3/avengers-endgame-poster-og-social-crop-15542720808371479664269.jpg'
+                        alt=''
+                        className='w-full h-full object-cover rounded-lg'
+                    />
+                    <div className='absolute left-5 bottom-5 w-full text-white mb-3'>
+                        <h2 className='font-bold text-3xl '>Avengers: Endgame</h2>
+                        <div className='flex items-center gap-x-3 mb-8'>
+                            <span className='py-2 px-4 border border-white rounded-md'>Adventure</span>
+                            <span className='py-2 px-4 border border-white rounded-md'>Adventure</span>
+                            <span className='py-2 px-4 border border-white rounded-md'>Adventure</span>
+                        </div>
+                        <button className='py-3 px-6 rounded-lg bg-primary text-white font-medium'>Watch Now</button>
+                    </div>
+                </div>
+            </section>
+
+            <section className='movies-layout page-container pb-20'>
+                <h2 className='capitalize text-white mb-10 text-3xl font-bold'>Now playing</h2>
+                <div className='movie-list grid grid-cols-4 gap-10 text-white'>
+                    <div className='movie-card rounded-lg p-3 bg-slate-800'>
+                        <img
+                            src='https://nld.mediacdn.vn/2019/4/3/avengers-endgame-poster-og-social-crop-15542720808371479664269.jpg'
+                            alt=''
+                            className='w-full h-[250px] object-cover rounded-lg mb-5'
+                        />
+                        <h3 className='text-xl font-bold mb-3'>Spiderman: Homecoming</h3>
+                        <div className='flex items-center justify-between text-sm opacity-50 mb-10'>
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className='py-3 px-6 rounded-lg capitalize bg-primary w-full'>Watch Now</button>
+                    </div>
+                </div>
+            </section>
+
+            <section className='movies-layout page-container pb-20'>
+                <h2 className='capitalize text-white mb-10 text-3xl font-bold'>Top Rated</h2>
+                <div className='movie-list grid grid-cols-4 gap-10 text-white'>
+                    <div className='movie-card rounded-lg p-3 bg-slate-800'>
+                        <img
+                            src='https://nld.mediacdn.vn/2019/4/3/avengers-endgame-poster-og-social-crop-15542720808371479664269.jpg'
+                            alt=''
+                            className='w-full h-[250px] object-cover rounded-lg mb-5'
+                        />
+                        <h3 className='text-xl font-bold mb-3'>Spiderman: Homecoming</h3>
+                        <div className='flex items-center justify-between text-sm opacity-50 mb-10'>
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className='py-3 px-6 rounded-lg capitalize bg-primary w-full'>Watch Now</button>
+                    </div>
+                </div>
+            </section>
+
+            <section className='movies-layout page-container pb-20'>
+                <h2 className='capitalize text-white mb-10 text-3xl font-bold'>Trending</h2>
+                <div className='movie-list grid grid-cols-4 gap-10 text-white'>
+                    <div className='movie-card rounded-lg p-3 bg-slate-800'>
+                        <img
+                            src='https://nld.mediacdn.vn/2019/4/3/avengers-endgame-poster-og-social-crop-15542720808371479664269.jpg'
+                            alt=''
+                            className='w-full h-[250px] object-cover rounded-lg mb-5'
+                        />
+                        <h3 className='text-xl font-bold mb-3'>Spiderman: Homecoming</h3>
+                        <div className='flex items-center justify-between text-sm opacity-50 mb-10'>
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className='py-3 px-6 rounded-lg capitalize bg-primary w-full'>Watch Now</button>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
