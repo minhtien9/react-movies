6178e7bb41ca810487b175bab7ab8e865fbc9c4b
DESKTOP-PEKI6LH\Lenovo <tienvm1@bytetech.io>
2022-12-04 10:27:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React from "react";
 import Banner from "./components/banner/Banner";
-import MovieList from "./components/movie/MovieList";
 import "swiper/scss";
+import HomePage from "./page/HomePage";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/layout/Main";
+import MoviePage from "./page/MoviePage";
 
 function App() {
     return (
         <React.Fragment>
-            <header className='header flex items-center justify-center gap-x-5 text-white py-10 mb-5'>
-                <span className='text-primary'>Home</span>
-                <span>Movies</span>
-            </header>
-
-            <Banner></Banner>
-
-            <section className='movies-layout page-container pb-20'>
-                <h2 className='capitalize text-white mb-10 text-3xl font-bold'>Now playing</h2>
-                <MovieList></MovieList>
-            </section>
-
-            <section className='movies-layout page-container pb-20'>
-                <h2 className='capitalize text-white mb-10 text-3xl font-bold'>Top Rated</h2>
-                <MovieList type='top_rated'></MovieList>
-            </section>
-
-            <section className='movies-layout page-container pb-20'>
-                <h2 className='capitalize text-white mb-10 text-3xl font-bold'>Trending</h2>
-                <MovieList type='popular'></MovieList>
-            </section>
+            <Routes>
+                <Route element={<Main></Main>}>
+                    <Route
+                        path='/'
+                        element={
+                            <>
+                                <Banner></Banner>
+                                <HomePage></HomePage>
+                            </>
+                        }></Route>
+                    <Route path='/movies' element={<MoviePage></MoviePage>}></Route>
+                </Route>
+            </Routes>
         </React.Fragment>
     );
 }
